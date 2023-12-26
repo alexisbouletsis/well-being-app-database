33a2e79144edd392ce216ae2c243e5ca343c77a9
Valia Pappa <valia.pp8@gmail.com>
2023-12-26 17:15:40 +0200

    Add connection with the database and medication page

---
+++ db_connection.php
@@ -0,0 +1,17 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "159ariadn3951#";
+$dbname = "well-being_app_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// // Check connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// } else {
+//     echo "Connected successfully to the database!";
+// }
+
+?>
